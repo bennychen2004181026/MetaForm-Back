ee72586bbf8fe7a3cb198b11a5575832551510ce
yansong <liyansong2abc@gmail.com>
2023-10-22 18:03:02 +1100

    improvement for pull request 2 comments

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import companyController = require('./company.controller');
+import staffController = require('./staff.controller');
+import authController = require('./auth.controller');
+
+export default { companyController, staffController, authController };
