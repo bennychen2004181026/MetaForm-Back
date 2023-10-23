66246f82d945e524cc78e0fc797b4b7dd7daa892
yansong <liyansong2abc@gmail.com>
2023-10-23 12:31:08 +1100

    delete comment

--- src/utils/winston.ts
+++ src/utils/winston.ts
@@ -20,11 +20,7 @@ const logger = createLogger({
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'prod') {
   logger.add(
     new transports.Console({
       format: format.simple(),
