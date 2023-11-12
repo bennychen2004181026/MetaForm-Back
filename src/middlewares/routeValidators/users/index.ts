97efe96beca68053e2fa161010d7791cc3e79543
Ben <ben83026@outlook.com>
2023-11-12 22:07:21 +1100

    Create forgotPasswordValidator middleware

--- src/middlewares/routeValidators/users/index.ts
+++ src/middlewares/routeValidators/users/index.ts
@@ -3,11 +3,13 @@ import checkUserExistence from '@middleware/routeValidators/users/user.checkUser
 import userInfosValidator from '@middleware/routeValidators/users/user.userInfosValidator'
 import completeAccountValidator from '@middleware/routeValidators/users/user.completeAccountValidator'
 import loginValidator from '@middleware/routeValidators/users/user.loginValidator'
+import forgotPasswordValidator from '@middleware/routeValidators/users/user.forgotPasswordValidator'
 
 export default {
     checkUserExistence,
     emailValidator,
     userInfosValidator,
     completeAccountValidator,
-    loginValidator
+    loginValidator,
+    forgotPasswordValidator
 }
