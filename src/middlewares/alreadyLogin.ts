f375ceacffdcf8131a49646d9d579cd7663fff3a
Ben <ben83026@outlook.com>
2023-11-12 18:58:22 +1100

    Create alreadyLogin middleware

---
+++ src/middlewares/alreadyLogin.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import Errors from '@errors/ClassError';
+
+const alreadyLogin = (req: Request, res: Response, next: NextFunction): void => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        // No token, so proceed to the intended route
+        return next();
+    }
+    const authToken = authorization.split(' ')[1];
+    const authType = authorization.split(' ')[0];
+
+    if (authType !== 'Bearer' || authorization.split(' ').length !== 2) {
+        return next(new Errors.AuthorizationError('Invalid authorization format', 'Header authorization'))
+    }
+
+    if (!authToken) {
+        return next(new Errors.AuthorizationError('Token is not provided or invalid', 'Authorization Token'))
+    }
+
+    if (!process.env.JWT_SECRET) {
+        return next(new Errors.EnvironmentError('JWT_SECRET is not defined.', 'env variable'));
+    }
+
+    try {
+        jwt.verify(authToken, process.env.JWT_SECRET);
+        return next(new Errors.AuthorizationError('You are already logged in.', 'Authorization'));
+    }
+    catch (error) {
+        next(error)
+    }
+};
+
+export default alreadyLogin
