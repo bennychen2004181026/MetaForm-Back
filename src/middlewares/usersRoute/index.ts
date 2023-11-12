a73c52bb9a3224c3ca557e19a1336ab346441618
Ben <ben83026@outlook.com>
2023-11-12 15:40:15 +1100

    Create generateToken and sendTokenAndUser middlewares

--- src/middlewares/usersRoute/index.ts
+++ src/middlewares/usersRoute/index.ts
@@ -1,5 +1,9 @@
 import verifyToken from './user.verifyToken'
+import sendTokenAndUser from './user.sendTokenAndUser'
+import generateToken from './user.generateToken'
 
 export default {
-    verifyToken
+    verifyToken,
+    sendTokenAndUser,
+    generateToken
 }
