d07b8f53a058df7c5ecde6e29574f458c9988054
Chris Zhang <29418695sydney@gmail.com>
2023-11-15 16:37:23 +1100

    resolve conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,14 @@
 import Router from 'express';
 import companyRouter from './company.route';
-<<<<<<< HEAD
 import questionRouter from './question.route';
 
 const v1Router = Router();
 v1Router.use('/companies', companyRouter);
 v1Router.use('/questions', questionRouter);
-=======
 import formRouter from './form.route';
 
 const router = Router();
 router.use('/companies', companyRouter);
 router.use('/forms', formRouter);
->>>>>>> 787684725526852e75579fb5c796ffb5ccb5ae8e
 
 export default router;
