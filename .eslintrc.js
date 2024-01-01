cc87364fd131a3a3fc973750e01709a49ae4307b
Billy <billychuim@gmail.com>
2024-01-01 19:07:30 +1100

    fix: add the husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
         tsconfigRootDir: __dirname,
-        // "project": "**/tsconfig.json"
+        project: './tsconfig.json',
     },
     parser: '@typescript-eslint/parser',
     settings: {
